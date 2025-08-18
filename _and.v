44b7b081b422b59a16fdb249010d83492b161c87
Vi-Nat <157328098+Vi-Natareno@users.noreply.github.com>
2025-08-18 17:21:44 -0600

    añade practica 1, compuerta AND

---
+++ _and.v
@@ -0,0 +1,12 @@
+//Definicion del modulo y sus i/p
+module _and (input A, input B, output C); //puede comenzar solo con guion bajo, mayus o minus
+//2. declara señales/ elementos internos
+
+
+//3. Comportamiento del modulo 
+//(asignaciones, instancias, conexiones)
+//
+assign C = A&B;
+ 
+ 
+ endmodule
